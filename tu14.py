b8a26bf0e66008dd55c9af2542fd4dfd13e912f2
louico <450125293@qq.com>
2019-01-18 17:51:34 +0900

    a commit

--- tu14.py
+++ tu14.py
@@ -3,9 +3,9 @@ path = "./programtest.txt"
 def read_program(path):
     with open(path) as f:
         program = f.read()
-    program = program.split("\n")
+    programarray = program.split("\n")
     row = len(program)
-    return program,row
+    return program,programarray,row
 def find_char(text,char):
     num = 0
     for i in text :
@@ -13,5 +13,18 @@ def find_char(text,char):
             if j == char:
                 num+=1
     return num
-program,row = read_program(path)
+def find_str(text,str_to_find):
+    result=[]
+
+    for i in range(len(text)):
+        if text[i].find(str_to_find)>0:
+            #str1.find(str) return the position of str in str1
+            tmp = str(i)+" "+text[i]
+            result.append(tmp)
+    return result
+
+def find_duplicate(text):
+    pass
+text,program,row = read_program(path)
 print(find_char(program,";"))
+#print(find_str(program,"main"))
